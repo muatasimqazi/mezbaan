875913a74298a4934e12b1ff8a610f5cc4534bf2
Bilal <bilal.tahir89@gmail.com>
2018-06-09 16:17:49 -0700

    search bar refactor

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import AppBarTop from './components/AppBarTop';
+import SearchBar from './components/SearchBar';
 import Grid from '@material-ui/core/Grid';
 import { Paper } from '@material-ui/core';
 import Hero from './images/hero-2.png';
@@ -11,6 +12,10 @@ class App extends Component {
     return (
       <div className="App">
         <AppBarTop />
+        <SearchBar
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => console.log('onRequestSearch')}
+        />
         <Grid container spacing={24}>
           <Grid item xs={12}>
               <img src={Hero} alt="" />
--- src/components/AppBarTop.js
+++ src/components/AppBarTop.js
@@ -7,7 +7,6 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import SearchBar from './SearchBar';
 
 const styles = {
   root: {
@@ -36,7 +35,6 @@ function AppBarTop(props) {
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
-        <SearchBar />
       </AppBar>
     </div>
   );
