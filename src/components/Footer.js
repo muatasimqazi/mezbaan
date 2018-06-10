124406f83b5df36c9d31988bcdcc89246cfbd31a
Muatasim Qazi <muatasimqazi@gmail.com>
2018-06-10 11:41:23 -0700

    footer cols

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,29 +1,53 @@
 import React, { Component } from 'react';
 import { Paper, Typography } from '@material-ui/core';
+import { Container, Row, Col } from 'react-grid-system';
 
 const styles = {
-    footer: {
-        height: '20vh',
-        background: '#66717d',
-        padding: 50,
-    },
-    content: {
-        color: 'white',
-    }
+footer: {
+    height: '20vh',
+    background: '#66717d',
+    padding: 50,
+},
+content: {
+    color: 'white',
+}
 }
 class Footer extends Component {
-    render() {
-        return (
-            <div style={styles.footer}>
-                <Typography variant="headline" component="h3" style={styles.content}>
+render() {
+    return (
+        <div style={styles.footer}>
+            <Container>
+                <Row>
+                    <Col sm={4} debug>
+                        <Typography variant="headline" component="h3" style={styles.content}>
+                            This is...
+            </Typography>
+                        <Typography component="p" style={styles.content}>
+                            Lorem impsum lorem ipsum lorem ipsum.
+            </Typography>
+                    </Col>
+
+                    <Col sm={4} debug>
+                        <Typography variant="headline" component="h3" style={styles.content}>
+                            This is...
+            </Typography>
+                        <Typography component="p" style={styles.content}>
+                            Lorem impsum lorem ipsum lorem ipsum.
+            </Typography>
+                    </Col>
+                </Row>
+                <Col sm={4} debug>
+                    <Typography variant="headline" component="h3" style={styles.content}>
                         This is...
-                </Typography>
+            </Typography>
                     <Typography component="p" style={styles.content}>
                         Lorem impsum lorem ipsum lorem ipsum.
-                </Typography>
-            </div>
-        );
-    }
+            </Typography>
+                </Col>
+            </Container>
+        </div>
+    );
+}
 }
 
 export default Footer;
